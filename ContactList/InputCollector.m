0891403252a59705be37b4a9d8800a4d937e0550
Leo Moon <leomoondev@gmail.com>
2017-02-10 13:02:14 -0800

    Initial Commit

---
+++ ContactList/InputCollector.m
@@ -0,0 +1,16 @@
+//
+//  InputCollector.m
+//  ContactList
+//
+//  Created by Hyung Jip Moon on 2017-02-10.
+//  Copyright © 2017 leomoon. All rights reserved.
+//
+
+#import "InputCollector.h"
+
+@implementation InputCollector
+
+-(NSString *)inputForPrompt:(NSString *)promptString {
+    
+}
+@end
